561a725ad37c4c6accace9ef2f80e6d1b3378aab
josh3287 <91765645+josh3287@users.noreply.github.com>
2024-08-08 10:14:02 -0400

    Create Main

---
+++ MAainMadLibs.py
@@ -0,0 +1,13 @@
+print("Welcome to MadLibs! Fill in the corresponding fields below to tell me a story!")
+
+
+adj = input("Insert adjective: ")
+adj2 = input("Insert adjective: ")
+noun = input("Length of time in months, days or years: ")
+
+
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+    it is {adj2}. It has taken me {noun} to get comfortable with Python"
+
+print(madlib)
